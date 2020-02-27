f7132aedeaa24299c4bbfc038d2b5ff5876d449f
igRock <topchiy.greg@yandex.ru>
2020-02-27 21:58:28 +0300

    unit 3-4 - regex refactoring

--- unit3-4/src/main/java/java_parsers/KeywordCounter.java
+++ unit3-4/src/main/java/java_parsers/KeywordCounter.java
@@ -10,8 +10,11 @@ import java.util.stream.Collectors;
 public class KeywordCounter {
     public static Map<String, Long> getResult(String javaText) {
         return Arrays.stream(javaText
-                .replaceAll("((['\"])(?:(?!\\2|\\\\).|\\\\.)*\\2)|\\/\\/[^\\n]*|\\/\\*(?:[^*]|\\*(?!\\/))*\\*\\/", "") //Удаляю все комментарии
-                .split("[\\s|\\(|=|,|\\)|;]")) //дроблю на слова
+//                .replaceAll("((['\"])(?:(?!\\2|\\\\).|\\\\.)*\\2)|\\/\\/[^\\n]*|\\/\\*(?:[^*]|\\*(?!\\/))*\\*\\/", "") //Удаляю все одну строку
+                .replaceAll("([\"'])(?:(?=(\\\\?))\\2.)*?\\1", "") // Убираем значения строк во всех видах кавычек
+                .replaceAll("(?s:\\/\\*.*?\\*\\/)|\\/\\/.*", "") // Убираем блочные и строчные комментарии
+                .replaceAll("[^\\w\\s]", "") // Убираем лишние символы кроме слов и пробелов
+                .split(" ")) // Дроблю на слова
                 .filter(JavaUtils::isJavaKeyword)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
     }
