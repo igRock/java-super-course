0f07c249ff7d1fd079e0bb81ba72ba8a796252e5
g.topchiy <g.topchiy@tinkoff.ru>
2020-03-06 18:27:02 +0300

    unit 5 file explorer update

--- unit5/src/main/java/FileExplorer.java
+++ unit5/src/main/java/FileExplorer.java
@@ -4,79 +4,79 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class FileExplorer {
-    String globalPath;
+
+    Path globalPath;
+    //    String globalPath;
 
     public FileExplorer() {
-        this.globalPath = new File("").getAbsolutePath();
+        this.globalPath = Path.of("");
+        //        this.globalPath = new File("").getAbsolutePath();
     }
 
-    public String showItems() {
+    public String showItems(Path path) throws IOException {
         StringBuilder builder = new StringBuilder();
-        File file = new File(globalPath);
-        if (file.isDirectory() && file.listFiles() != null) {
-            //noinspection ConstantConditions
-            for (File item : file.listFiles()) {
-                if (item.isDirectory()) {
-                    builder.append(item.getName()).append(File.separator).append("\r\n");
-                } else {
-                    builder.append(item.getName()).append("\r\n");
-                }
+        Files.list(path).forEach(item -> {
+            builder.append(item.getFileName().toString());
+            if (Files.isDirectory(item)) {
+                builder.append(File.separator);
             }
-        } else builder.append(file.getName());
+            builder.append("\r\n");
+        });
         return builder.toString();
     }
 
-    public String showItems(String path) {
-        globalPath = path;
-        return showItems();
+    public String showItems() throws IOException {
+        return showItems(globalPath);
     }
 
-    public void createTextFile(String text, String fileName) {
+    public void createTextFile(String text, String fileName) throws IOException {
         createTextFile(text, fileName, globalPath);
     }
 
-    public void createTextFile(String text, String fileName, String path) {
-        File pathFile = new File(path);
-        if (!pathFile.exists() || !pathFile.isDirectory()) {
-            pathFile.mkdirs();
+    public void createTextFile(String text, String fileName, Path path) throws IOException {
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            Files.createDirectories(path);
         }
-        try (BufferedWriter writer =
-            new BufferedWriter(new FileWriter(pathFile + File.separator + fileName + ".txt"))) {
-            writer.write(text);
-            writer.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Files.createFile(Paths.get(path.toString(), fileName.concat(".txt")));
     }
 
-    public void removeFile(String path) {
-        File file = Path.of(path).toFile();
-        if (!file.delete()) throw new RuntimeException("File was not deleted");
+    public void removeFile(Path path) throws IOException {
+        Files.delete(path);
     }
 
-    public void editTextFile(String path, String newText) {
-        try (FileWriter fw = new FileWriter(path, true);
-            BufferedWriter bw = new BufferedWriter(fw);
-            PrintWriter out = new PrintWriter(bw)) {
-            out.println(newText);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public void editTextFile(Path path, String newText) throws IOException {
+        Files.write(path, Collections.singleton(newText), StandardCharsets.UTF_8);
+
+//        try (FileWriter fw = new FileWriter(path, true);
+//            BufferedWriter bw = new BufferedWriter(fw);
+//            PrintWriter out = new PrintWriter(bw)) {
+//
+//            out.println(newText);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
-    public String readTextFile(String path) {
-        File file;
-        try (FileReader reader = new FileReader(file = new File(path))) {
-            char[] buf = new char[(int) file.length()];
-            //noinspection ResultOfMethodCallIgnored
-            reader.read(buf);
-            return new String(buf).trim();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "";
+    public String readTextFile(Path path) throws IOException {
+        return Files.readAllLines(path).stream().collect(Collectors.joining("\n"));
+
+//        File file;
+//        try (FileReader reader = new FileReader(file = new File(path))) {
+//            char[] buf = new char[(int) file.length()];
+//            //noinspection ResultOfMethodCallIgnored
+//            reader.read(buf);
+//            return new String(buf).trim();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return "";
     }
 }
--- unit5/src/main/java/FileExplorerApp.java
+++ unit5/src/main/java/FileExplorerApp.java
@@ -1,30 +1,39 @@
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileExplorerApp {
 
     public static void main(String[] args) {
-        FileExplorer explorer = new FileExplorer();
-        System.out.println("Файлы в корне приложения:");
-        System.out.println(explorer.showItems());
+        try {
+            FileExplorer explorer = new FileExplorer();
+            System.out.println("Файлы в корне приложения:");
+            System.out.println(explorer.showItems());
 
-        System.out.println("Файлы по определенному пути:");
-        System.out.println(explorer.showItems("unit5/src/main/java"));
+            System.out.println("Файлы по определенному пути:");
+            System.out.println(explorer.showItems(Path.of("unit5/src/main/java")));
 
-        String fileName = "note";
-        String path = "unit5/src/main/resources";
-        System.out.println("Создаем файл");
-        explorer.createTextFile("Какой-то текст", fileName, path);
+            String fileName = "note";
+            Path path = Path.of("unit5/src/main/resources");
+            System.out.println("Создаем файл");
+            explorer.createTextFile("Какой-то текст", fileName, path);
 
-        System.out.println("Читаем созданный файл:");
-        System.out.println(explorer.readTextFile("unit5/src/main/resources/note.txt"));
+            Path filePath = Path.of("unit5/src/main/resources/note.txt");
+            System.out.println("Читаем созданный файл:");
+            System.out.println(explorer.readTextFile(filePath));
 
-        System.out.println("Редактируем созданный файл:");
-        explorer.editTextFile("unit5/src/main/resources/note.txt", "\r\nnew text!");
+            System.out.println("Редактируем созданный файл:");
+            explorer.editTextFile(filePath, "\r\nnew text!");
 
-        System.out.println("Читаем отредактированный файл:");
-        System.out.println(explorer.readTextFile("unit5/src/main/resources/note.txt"));
+            System.out.println("Читаем отредактированный файл:");
+            System.out.println(explorer.readTextFile(filePath));
 
-        System.out.println("Удаляем созданный файл");
-        explorer.removeFile("unit5/src/main/resources/note.txt");
+//            System.out.println("Удаляем созданный файл");
+//            explorer.removeFile(filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
