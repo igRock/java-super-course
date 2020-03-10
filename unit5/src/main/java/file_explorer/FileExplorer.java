2d8e7a0c1f9f2a4d736c1db2dc377d91e8c1801d
g.topchiy <g.topchiy@tinkoff.ru>
2020-03-10 17:21:53 +0300

    unit 5 cli explorer

--- unit5/src/main/java/file_explorer/FileExplorer.java
+++ unit5/src/main/java/file_explorer/FileExplorer.java
@@ -1,29 +1,85 @@
 package file_explorer;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class FileExplorer {
 
-    Path globalPath;
-    //    String globalPath;
+    private Path globalPath;
+    private static Scanner scanner = new Scanner(System.in);
 
     public FileExplorer() {
         this.globalPath = Path.of("");
-        //        this.globalPath = new File("").getAbsolutePath();
     }
 
-    public String showItems(Path path) throws IOException {
+    public void run() {
+        boolean isRunning = true;
+        System.out.println("The app is running. Type \"help\" to list all commands");
+        while (isRunning) {
+            String input = scanner.nextLine().trim();
+            String[] inputParts = input.split(" ");
+            String action = inputParts[0];
+            try {
+                switch (action) {
+                    case ("help"):
+                        printListOfCommands();
+                        break;
+                    case ("create"):
+                        createTextFile(inputParts);
+                        break;
+                    case ("edit"):
+                        editTextFile(inputParts);
+                        break;
+                    case ("read"):
+                        readTextFile(inputParts);
+                        break;
+                    case ("delete"):
+                        removeFile(inputParts);
+                        break;
+                    case ("list"):
+                        showItems(inputParts);
+                        break;
+                    case ("exit"):
+                        isRunning = false;
+                        break;
+                    default:
+                        break;
+                }
+            } catch (IOException ex) {
+                System.out.println("Wrong path");
+            }
+        }
+    }
+
+    private void printListOfCommands() {
+        StringBuilder output = new StringBuilder();
+        output.append("create - creates text file i.e. - \"create textFile.txt unit5/src/main/resources\"\n\n");
+        output.append("edit - edits text file i.e. - \"edit unit5/src/main/resources/textFile.txt Текст файла\"\n\n");
+        output.append("read - reads text file i.e. - \"read unit5/src/main/resources/textFile.txt\"\n\n");
+        output.append("delete - deletes file i.e. - \"delete unit5/src/main/resources/textFile.txt\"\n\n");
+        output.append("list - lists files in directory i.e. - \"list unit5/src/main/resources\"\n\n");
+        output.append("exit - exits app\n\n");
+        output.append("help - lists all commands\n\n");
+        System.out.println(output.toString());
+    }
+
+    private void showItems(String[] inputParts) throws IOException {
+        if (inputParts.length > 2) {
+            System.out.println("Wrong arguments");
+            return;
+        }
+        Path path = inputParts.length == 1 ? globalPath : Path.of(inputParts[1]);
+        showItems(path);
+    }
+
+    private void showItems(Path path) throws IOException {
         StringBuilder builder = new StringBuilder();
         Files.list(path).forEach(item -> {
             builder.append(item.getFileName().toString());
@@ -32,18 +88,19 @@ public class FileExplorer {
             }
             builder.append("\r\n");
         });
-        return builder.toString();
+        System.out.println(builder.toString());
     }
 
-    public String showItems() throws IOException {
-        return showItems(globalPath);
+    private void createTextFile(String[] inputParts) throws IOException {
+        if (inputParts.length < 2) {
+            System.out.println("Wrong arguments");
+            return;
+        }
+        Path path = inputParts.length == 2 ? globalPath : Path.of(inputParts[2]);
+        createTextFile(inputParts[1], path);
     }
 
-    public void createTextFile(String text, String fileName) throws IOException {
-        createTextFile(text, fileName, globalPath);
-    }
-
-    public void createTextFile(String text, String fileName, Path path) throws IOException {
+    private void createTextFile(String fileName, Path path) throws IOException {
         if (!Files.exists(path) || !Files.isDirectory(path)) {
             Files.createDirectories(path);
         }
@@ -52,18 +109,34 @@ public class FileExplorer {
             return;
         }
         Path newFile = Files.createFile(filePath);
-        Files.write(newFile, Collections.singleton(text), StandardCharsets.UTF_8);
+        System.out.println("Enter something to file");
+        Files.write(newFile, Collections.singleton(scanner.nextLine()), StandardCharsets.UTF_8);
+        System.out.println("File created");
     }
 
-    public void removeFile(Path path) throws IOException {
-        Files.delete(path);
+    private void removeFile(String[] inputParts) throws IOException {
+        if (inputParts.length != 2) {
+            System.out.println("Wrong arguments");
+            return;
+        }
+        Files.delete(Path.of(inputParts[1]));
+        System.out.println("File deleted");
     }
 
-    public void editTextFile(Path path, String newText) throws IOException {
-        Files.write(path, Collections.singleton(newText), StandardCharsets.UTF_8);
+    private void editTextFile(String[] inputParts) throws IOException {
+        if (inputParts.length < 3) {
+            System.out.println("Wrong arguments");
+            return;
+        }
+        System.out.println("Enter something to file");
+        Files.write(Path.of(inputParts[1]), Collections.singleton(scanner.nextLine()), StandardCharsets.UTF_8);
     }
 
-    public String readTextFile(Path path) throws IOException {
-        return Files.readAllLines(path).stream().collect(Collectors.joining("\n"));
+    private void readTextFile(String[] inputParts) throws IOException {
+        if (inputParts.length != 2) {
+            System.out.println("Wrong arguments");
+            return;
+        }
+        System.out.println(Files.readAllLines(Path.of(inputParts[1])).stream().collect(Collectors.joining("\n")));
     }
 }
--- unit5/src/main/java/file_explorer/FileExplorerApp.java
+++ unit5/src/main/java/file_explorer/FileExplorerApp.java
@@ -1,42 +1,9 @@
 package file_explorer;
 
-import java.io.IOException;
-import java.nio.file.Path;
-
 public class FileExplorerApp {
 
-    public static final String FILE_NAME = "note.txt";
-    public static final String RESOURCES_PATH = "unit5/src/main/resources";
-    public static final String ROOT_PATH = "unit5/src/main/java";
-
     public static void main(String[] args) {
-        try {
-            FileExplorer explorer = new FileExplorer();
-            System.out.println("Файлы в корне приложения:");
-            System.out.println(explorer.showItems());
-
-            System.out.println("Файлы по определенному пути:");
-            System.out.println(explorer.showItems(Path.of(ROOT_PATH)));
-
-            Path path = Path.of(RESOURCES_PATH);
-            System.out.println("Создаем файл");
-            explorer.createTextFile("Какой-то текст", FILE_NAME, path);
-
-            Path filePath = Path.of(RESOURCES_PATH, FILE_NAME);
-            System.out.println("\nЧитаем созданный файл:");
-            System.out.println(explorer.readTextFile(filePath));
-
-            System.out.println("Редактируем созданный файл:");
-            explorer.editTextFile(filePath, "new text!");
-
-            System.out.println("\nЧитаем отредактированный файл:");
-            System.out.println(explorer.readTextFile(filePath));
-
-            System.out.println("Удаляем созданный файл");
-            explorer.removeFile(filePath);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        FileExplorer explorer = new FileExplorer();
+        explorer.run();
     }
 }
